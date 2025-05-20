62bce98256c4790e5cacfe7b85b828bdd3b7df70
miriam <202104686@alu.comillas.edu>
2025-05-20 21:29:10 +0200

    Changes by María

---
+++ proyectofinal/src/main/java/com/icai/proyectofinal/model/user/LoginRequest.java
@@ -0,0 +1,10 @@
+package com.icai.proyectofinal.model.user;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank
+        String email,
+        @NotBlank
+        String password
+) {}
